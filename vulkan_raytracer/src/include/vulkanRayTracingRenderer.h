d22d2c5cde2ef2de0d956c5765e2b17809a3390a
Tongwei Dai <tongweid@gmail.com>
2023-11-09 20:28:32 -0500

    vk WIP

--- vulkan_raytracer/src/include/vulkanRayTracingRenderer.h
+++ vulkan_raytracer/src/include/vulkanRayTracingRenderer.h
@@ -41,34 +41,75 @@ namespace PTS {
 			auto const& operator*() const {
 				return handle.operator*();
 			}
+            auto* get() const {
+                return handle.get();
+            }
+            auto const* get() {
+                return handle.get();
+            }
 		};
 		struct VulkanInsInfo : VulkanInfo<vk::UniqueInstance> {
 			std::vector<char const*> device_exts;
 			std::vector<char const*> layers;
 		};
 		struct VulkanDeviceInfo : VulkanInfo<vk::UniqueDevice> {
+            vk::PhysicalDevice physical_device;
 			unsigned queue_family_idx{ 0 };
 		};
 		struct VulkanCmdPoolInfo : VulkanInfo<vk::UniqueCommandPool> {
 			vk::Queue queue;
 		};
-
+        struct VulkanBufferInfo : VulkanInfo<vk::UniqueBuffer> {
+            vk::UniqueDeviceMemory mem;
+        };
+        struct VulkanImageInfo : VulkanInfo<vk::UniqueImage> {
+            vk::UniqueDeviceMemory mem;
+            vk::UniqueImageView view;
+            vk::Format format;
+        };
+        struct VulkanRenderPassInfo : VulkanInfo<vk::UniqueRenderPass> {
+            vk::Format color_fmt;
+            vk::Format depth_fmt;
+        };
+        struct VulkanFrameBufferInfo : VulkanInfo<vk::UniqueFramebuffer> {
+            VulkanImageInfo color_tex;
+            VulkanImageInfo depth_tex;
+        };
+        struct VulkanPipelineInfo : VulkanInfo<vk::UniquePipeline> {
+            vk::UniquePipelineLayout layout;
+        };
+        
+#define REQUIRES_INFO(...) // only for documentation
         [[nodiscard]] auto create_instance() -> tl::expected<VulkanInsInfo, std::string>;
         [[nodiscard]] auto create_device() -> tl::expected<VulkanDeviceInfo, std::string>;
-		[[nodiscard]] auto create_cmd_pool()->tl::expected<VulkanCmdPoolInfo, std::string>;
-		[[nodiscard]] auto create_frame_buffer() -> tl::expected<void, std::string>;
+        [[nodiscard]] auto create_cmd_pool() -> tl::expected<VulkanCmdPoolInfo, std::string>;
+        [[nodiscard]] auto create_buffer(
+            vk::BufferUsageFlags usage_flags,
+            vk::MemoryPropertyFlags prop_flags, 
+            vk::DeviceSize size,
+            void* data
+        ) -> tl::expected<VulkanBufferInfo, std::string>;
+        
+        [[nodiscard]] auto do_work_now(VulkanCmdPoolInfo const& cmd, vk::UniqueCommandBuffer cmd_buf)
+            -> tl::expected<void, std::string>;
+        
+        [[nodiscard]] auto create_tex(
+            vk::Format fmt,
+            unsigned width, unsigned height,
+            vk::ImageUsageFlags usage_flags,
+            vk::MemoryPropertyFlags prop_flags,
+            vk::ImageAspectFlags aspect_flags,
+            vk::ImageLayout layout
+        ) -> tl::expected<VulkanImageInfo, std::string>;
 
-        // prepare shared texture between vulkan and opengl
-        [[nodiscard]] auto prepared_shared_tex() -> tl::expected<void, std::string>;
+        [[nodiscard]] auto create_render_pass() -> tl::expected<VulkanRenderPassInfo, std::string>;
+        [[nodiscard]] auto create_frame_buf() -> tl::expected<VulkanFrameBufferInfo, std::string>;
+        [[nodiscard]] auto create_pipeline() -> tl::expected<VulkanPipelineInfo, std::string>;
 
 		VulkanInsInfo m_vk_ins;
 		VulkanDeviceInfo m_vk_device;
 		VulkanCmdPoolInfo m_vk_cmd_pool;
-
-		struct {
-			vk::UniqueImage image;
-			vk::UniqueDeviceMemory mem;
-			vk::UniqueImageView view;
-		} m_vk_frame_buf;
+        VulkanRenderPassInfo m_vk_render_pass;
+        VulkanFrameBufferInfo m_vk_frame_buf;
     };
 }
--- vulkan_raytracer/src/vulkanRayTracingRenderer.cpp
+++ vulkan_raytracer/src/vulkanRayTracingRenderer.cpp
@@ -2,7 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-constexpr float k_quad_data_pos_uv[] = {
+static constexpr float k_quad_data_pos_uv[] = {
 	// First triangle (positions)
 	-1.0f, -1.0f, 0.0f,
 	1.0f, -1.0f, 0.0f,
@@ -20,6 +20,10 @@ constexpr float k_quad_data_pos_uv[] = {
 	1.0f, 1.0f,
 	0.0f, 1.0f
 };
+
+static constexpr int k_max_width = 4000;
+static constexpr int k_max_height = 4000;
+
 
 PTS::VulkanRayTracingRenderer::VulkanRayTracingRenderer(RenderConfig config)
 	: Renderer{config, "Vulkan Ray Tracer"} {}
@@ -66,6 +70,52 @@ auto PTS::VulkanRayTracingRenderer::init(ObserverPtr<Application> app) noexcept
 	TL_CHECK(Renderer::init(app));
 	TL_TRY_ASSIGN(m_vk_ins, create_instance());
 	TL_TRY_ASSIGN(m_vk_device, create_device());
+    TL_TRY_ASSIGN(m_vk_cmd_pool, create_cmd_pool());
+    
+    // upload vertex data to VRAM
+    {
+        VulkanBufferInfo staging_buffer;
+        TL_TRY_ASSIGN(staging_buffer, create_buffer(
+            vk::BufferUsageFlagBits::eTransferSrc,
+            vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent,
+            sizeof(k_quad_data_pos_uv),
+            const_cast<void*>(static_cast<void const*>(k_quad_data_pos_uv))
+        ));
+        VulkanBufferInfo vertex_buffer;
+        TL_TRY_ASSIGN(vertex_buffer, create_buffer(
+            vk::BufferUsageFlagBits::eTransferDst | vk::BufferUsageFlagBits::eVertexBuffer,
+            vk::MemoryPropertyFlagBits::eDeviceLocal,
+            sizeof(k_quad_data_pos_uv),
+            nullptr
+        ));
+
+        auto copy_cmd = vk::UniqueCommandBuffer {};
+        try {
+            auto cmds = m_vk_device->allocateCommandBuffersUnique(
+                vk::CommandBufferAllocateInfo{
+                    m_vk_cmd_pool.handle.get(),
+                    vk::CommandBufferLevel::ePrimary,
+                    1
+                }
+            );
+            cmds[0]->begin(vk::CommandBufferBeginInfo{});
+            cmds[0]->copyBuffer(staging_buffer.handle.get(), vertex_buffer.handle.get(), vk::BufferCopy {
+                0, 0, sizeof(k_quad_data_pos_uv)
+            });
+            cmds[0]->end();
+            copy_cmd = std::move(cmds[0]);
+        } catch (vk::SystemError& err) {
+            return TL_ERROR(err.what());
+        }
+
+        TL_CHECK_AND_PASS(do_work_now(m_vk_cmd_pool, std::move(copy_cmd)));
+    }
+
+    // create render pass
+    TL_TRY_ASSIGN(m_vk_render_pass, create_render_pass());
+    // create frame buffer
+    TL_TRY_ASSIGN(m_vk_frame_buf, create_frame_buf());
+
 	return {};
 }
 
@@ -187,13 +237,17 @@ auto PTS::VulkanRayTracingRenderer::create_device() -> tl::expected<VulkanDevice
 			}
 		);
 
-		return VulkanDeviceInfo{{std::move(dev)}, queue_family_index };
+		return VulkanDeviceInfo{{std::move(dev)}, std::move(physical_device), queue_family_index };
 	} catch (vk::SystemError& err) {
 		return TL_ERROR(err.what());
 	}
 }
 
 auto PTS::VulkanRayTracingRenderer::create_cmd_pool() -> tl::expected<VulkanCmdPoolInfo, std::string> {
+    if(!m_vk_device) {
+        return TL_ERROR("device not created");
+    }
+
 	try {
 		auto cmd_pool = m_vk_device->createCommandPoolUnique(
 			vk::CommandPoolCreateInfo{
@@ -209,6 +263,296 @@ auto PTS::VulkanRayTracingRenderer::create_cmd_pool() -> tl::expected<VulkanCmdP
 	}
 }
 
-auto PTS::VulkanRayTracingRenderer::create_frame_buffer() -> tl::expected<void, std::string> {
-	return {};
+auto PTS::VulkanRayTracingRenderer::create_buffer(vk::BufferUsageFlags usage_flags, vk::MemoryPropertyFlags prop_flags,
+	vk::DeviceSize size, void* data) -> tl::expected<VulkanBufferInfo, std::string> {
+    if(!m_vk_device) {
+        return TL_ERROR("device not created");
+    }
+
+    try {
+        auto buffer = m_vk_device->createBufferUnique(
+            vk::BufferCreateInfo{
+                vk::BufferCreateFlags{},
+                size,
+                usage_flags,
+                vk::SharingMode::eExclusive,
+                0,
+                nullptr
+            }
+        );
+
+        // get property index
+        auto const mem_req = m_vk_device->getBufferMemoryRequirements(*buffer);
+        auto const mem_props = m_vk_device.physical_device.getMemoryProperties();
+        auto mem_type_idx = std::numeric_limits<uint32_t>::max();
+        for (auto i = 0u; i < mem_props.memoryTypeCount; ++i) {
+            if ((mem_req.memoryTypeBits & (1 << i)) && (mem_props.memoryTypes[i].propertyFlags & prop_flags) == prop_flags) {
+                mem_type_idx = i;
+                break;
+            }
+        }
+        if (mem_type_idx == std::numeric_limits<uint32_t>::max()) {
+            return TL_ERROR("no suitable memory type found");
+        }
+        
+        auto mem = m_vk_device->allocateMemoryUnique(
+            vk::MemoryAllocateInfo{
+                mem_req.size,
+                mem_type_idx
+            }
+        );
+        
+        if (data) {
+            auto const mapped = m_vk_device->mapMemory(*mem, 0, size);
+            memcpy(mapped, data, size);
+            m_vk_device->unmapMemory(*mem);
+        }
+
+        m_vk_device->bindBufferMemory(*buffer, *mem, 0);
+        return VulkanBufferInfo{{std::move(buffer)}, std::move(mem) };
+    } catch (vk::SystemError& err) {
+        return TL_ERROR(err.what());
+    }
+}
+
+auto PTS::VulkanRayTracingRenderer::do_work_now(VulkanCmdPoolInfo const& cmd, vk::UniqueCommandBuffer cmd_buf)
+-> tl::expected<void, std::string> {
+    if(!m_vk_device) {
+        return TL_ERROR("device not created");
+    }
+
+    try {
+        auto const fence = m_vk_device->createFenceUnique(vk::FenceCreateInfo{});
+        cmd.queue.submit(
+            vk::SubmitInfo{
+                0, nullptr, nullptr,
+                1, &cmd_buf.get()
+            },
+            *fence
+        );
+
+        auto const res = m_vk_device->waitForFences(*fence, true, std::numeric_limits<uint64_t>::max());
+        if (res != vk::Result::eSuccess) {
+            return TL_ERROR("failed to wait for fence");
+        }
+
+        m_vk_device->resetFences(*fence);
+        return {};
+    } catch (vk::SystemError& err) {
+        return TL_ERROR(err.what());
+    }
+}
+
+auto PTS::VulkanRayTracingRenderer::create_tex(vk::Format fmt, unsigned width, unsigned height, vk::ImageUsageFlags usage_flags,
+vk::MemoryPropertyFlags prop_flags, vk::ImageAspectFlags aspect_flags, vk::ImageLayout layout) -> tl::expected<VulkanImageInfo, std::string> 
+{
+	if(!m_vk_device) {
+        return TL_ERROR("device not created");
+    }
+
+    try {
+        auto img = m_vk_device->createImageUnique(
+            vk::ImageCreateInfo{
+                vk::ImageCreateFlags{},
+                vk::ImageType::e2D,
+                fmt,
+                vk::Extent3D{ width, height, 1 },
+                1,
+                1,
+                vk::SampleCountFlagBits::e1,
+                vk::ImageTiling::eOptimal,
+                usage_flags,
+                vk::SharingMode::eExclusive,
+                0,
+                nullptr,
+                layout
+            }
+        );
+        auto const mem_req = m_vk_device->getImageMemoryRequirements(*img);
+        auto mem = m_vk_device->allocateMemoryUnique(
+            vk::MemoryAllocateInfo{
+                mem_req.size,
+                m_vk_device.queue_family_idx
+            }
+        );
+        m_vk_device->bindImageMemory(*img, *mem, 0);
+        auto view = m_vk_device->createImageViewUnique(
+            vk::ImageViewCreateInfo{
+                vk::ImageViewCreateFlags{},
+                *img,
+                vk::ImageViewType::e2D,
+                fmt,
+                vk::ComponentMapping{},
+                vk::ImageSubresourceRange{
+                    aspect_flags,
+                    0, 1, 0, 1
+                }
+            }
+        );
+        return VulkanImageInfo{{std::move(img)}, std::move(mem), std::move(view) };
+    } catch (vk::SystemError& err) {
+        return TL_ERROR(err.what());
+    }
+}
+
+auto PTS::VulkanRayTracingRenderer::create_render_pass() -> tl::expected<VulkanRenderPassInfo, std::string> {
+    if (!m_vk_device) {
+        return TL_ERROR("device not created");
+    }
+
+    try {
+        auto const color_fmt = vk::Format::eR8G8B8A8Unorm;
+        auto const depth_fmt = vk::Format::eD32Sfloat;
+    
+        auto props = m_vk_device.physical_device.getFormatProperties(color_fmt);
+        if (!(props.optimalTilingFeatures & vk::FormatFeatureFlagBits::eColorAttachment)) {
+            return TL_ERROR("color attachment not supported");
+        }
+        props = m_vk_device.physical_device.getFormatProperties(depth_fmt);
+        if (!(props.optimalTilingFeatures & vk::FormatFeatureFlagBits::eDepthStencilAttachment)) {
+            return TL_ERROR("depth attachment not supported");
+        }
+
+        auto color_attch_desc = vk::AttachmentDescription{
+            vk::AttachmentDescriptionFlags{},
+            color_fmt,
+            vk::SampleCountFlagBits::e1,
+            vk::AttachmentLoadOp::eClear,
+            vk::AttachmentStoreOp::eStore,
+            vk::AttachmentLoadOp::eDontCare,
+            vk::AttachmentStoreOp::eDontCare,
+            vk::ImageLayout::eUndefined,
+            vk::ImageLayout::eTransferSrcOptimal
+        };
+        auto depth_attch_desc = vk::AttachmentDescription{
+            vk::AttachmentDescriptionFlags{},
+            depth_fmt,
+            vk::SampleCountFlagBits::e1,
+            vk::AttachmentLoadOp::eClear,
+            vk::AttachmentStoreOp::eDontCare,
+            vk::AttachmentLoadOp::eDontCare,
+            vk::AttachmentStoreOp::eDontCare,
+            vk::ImageLayout::eUndefined,
+            vk::ImageLayout::eDepthStencilAttachmentOptimal
+        };
+        auto attch_descs = std::array<vk::AttachmentDescription, 2> {
+            color_attch_desc,
+            depth_attch_desc
+        };
+
+        auto color_attch_ref = vk::AttachmentReference{
+            0, vk::ImageLayout::eColorAttachmentOptimal
+        };
+        auto depth_attch_ref = vk::AttachmentReference{
+            1, vk::ImageLayout::eDepthStencilAttachmentOptimal
+        };
+        auto subpass_desc = vk::SubpassDescription{
+            vk::SubpassDescriptionFlags{},
+            vk::PipelineBindPoint::eGraphics,
+            0,
+            nullptr,
+            1,
+            &color_attch_ref,
+            nullptr,
+            &depth_attch_ref,
+            0,
+            nullptr
+        };
+        auto subpass_descs = std::array<vk::SubpassDescription, 1> {
+            subpass_desc
+        };
+
+        auto deps = std::array<vk::SubpassDependency, 2> {
+            vk::SubpassDependency{
+                VK_SUBPASS_EXTERNAL,
+                0,
+                vk::PipelineStageFlagBits::eBottomOfPipe,
+                vk::PipelineStageFlagBits::eColorAttachmentOutput,
+                vk::AccessFlagBits::eMemoryRead,
+                vk::AccessFlagBits::eColorAttachmentRead | vk::AccessFlagBits::eColorAttachmentWrite,
+                vk::DependencyFlagBits::eByRegion
+            },
+            vk::SubpassDependency{
+                0, VK_SUBPASS_EXTERNAL,
+                vk::PipelineStageFlagBits::eColorAttachmentOutput,
+                vk::PipelineStageFlagBits::eBottomOfPipe,
+                vk::AccessFlagBits::eColorAttachmentRead | vk::AccessFlagBits::eColorAttachmentWrite,
+                vk::AccessFlagBits::eMemoryRead,
+                vk::DependencyFlagBits::eByRegion
+            }
+        };
+
+        auto pass = m_vk_device->createRenderPassUnique(
+            vk::RenderPassCreateInfo {
+                vk::RenderPassCreateFlags{},
+                static_cast<uint32_t>(attch_descs.size()),
+                attch_descs.data(),
+                static_cast<uint32_t>(subpass_descs.size()),
+                subpass_descs.data(),
+                static_cast<uint32_t>(deps.size()),
+                deps.data()
+            }
+        );
+
+        return VulkanRenderPassInfo{{std::move(pass)}, color_fmt, depth_fmt };
+    } catch (vk::SystemError& err) {
+        return TL_ERROR(err.what());
+    }
+}
+
+
+auto PTS::VulkanRayTracingRenderer::create_frame_buf() -> tl::expected<VulkanFrameBufferInfo, std::string> {
+	if (!m_vk_device) {
+        return TL_ERROR("device not created");
+    }
+    if (!m_vk_render_pass) {
+        return TL_ERROR("render pass not created");
+    }
+    
+    try {
+        auto color_tex = VulkanImageInfo{};
+        TL_TRY_ASSIGN(color_tex, create_tex(
+            m_vk_render_pass.color_fmt,
+            m_config.width,
+            m_config.height,
+            vk::ImageUsageFlagBits::eColorAttachment | vk::ImageUsageFlagBits::eTransferSrc,
+            vk::MemoryPropertyFlagBits::eDeviceLocal,
+            vk::ImageAspectFlagBits::eColor,
+            vk::ImageLayout::eUndefined
+        ));
+        auto depth_tex = VulkanImageInfo{};
+        TL_TRY_ASSIGN(depth_tex, create_tex(
+            m_vk_render_pass.depth_fmt,
+            m_config.width,
+            m_config.height,
+            vk::ImageUsageFlagBits::eDepthStencilAttachment,
+            vk::MemoryPropertyFlagBits::eDeviceLocal,
+            vk::ImageAspectFlagBits::eDepth,
+            vk::ImageLayout::eUndefined
+        ));
+
+        auto attachments = std::array<vk::ImageView, 2> {
+            color_tex.view.get(),
+            depth_tex.view.get()
+        };
+
+        auto frame_buf = m_vk_device->createFramebufferUnique(
+            vk::FramebufferCreateInfo{
+                vk::FramebufferCreateFlags{},
+                *m_vk_render_pass,
+                static_cast<uint32_t>(attachments.size()),
+                attachments.data(),
+                m_config.width,
+                m_config.height,
+                1
+            }
+        );
+
+    } catch (vk::SystemError& err) {
+        return TL_ERROR(err.what());
+    }
+}
+
+auto PTS::VulkanRayTracingRenderer::create_pipeline() -> tl::expected<VulkanPipelineInfo, std::string> {
+    return {};
 }
