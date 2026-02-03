2eaf52ff92737903053f5e14e84d2021ae358cbf
Tongwei Dai <tongweid@gmail.com>
2026-02-02 18:25:50 -0800

    Remove unused dependencies

--- conanfile.py
+++ conanfile.py
@@ -37,8 +37,6 @@ class PTStudioConan(ConanFile):
         # Utility libraries
         self.requires("stb/[>=0]")
         self.requires("tinyobjloader/2.0.0-rc10")
-        self.requires("tcb-span/[>=0]")
-        self.requires("tl-expected/[>=0]")
         self.requires("doctest/[>=0]")
         self.requires("boost/[>=0]")
         # Shader toolchain (slangc)
